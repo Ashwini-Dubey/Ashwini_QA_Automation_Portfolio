6f4970e6569bdf02d971cd63ac65fcde1dca377b
Ashwini Dubey <ak213414@gmail.com>
2025-07-05 00:06:44 +0530

    Initial commit

---
+++ 01_Python_Basics/P01_BandNameGenerator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Band Name Generator
+
+This simple script generates a band name by combining the name of a city the user grew up in
+with the name of their pet. It’s an introductory Python project for practicing input/output
+operations and string manipulation.
+
+Author: Ashwini Dubey
+Date: July 2025
+"""
+
+print("Welcome to the Brand Name Generator.")
+user_city = input("What is the name of the city you have grew up in?\n")
+user_pet_name = input("What is the name of your pet?\n")
+band_name = user_city + " " + user_pet_name
+print("Your band name could be ",band_name)
+print("Thank you for using the Band Name Generator.")
