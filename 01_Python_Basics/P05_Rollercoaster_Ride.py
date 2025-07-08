88fbb9c706ea9f5489ecc1a1c4544a1f22db7729
Ashwini Dubey <ak213414@gmail.com>
2025-07-09 00:18:34 +0530

    If-Elif-Else_Nested

--- 01_Python_Basics/P05_Rollercoaster_Ride.py
+++ 01_Python_Basics/P05_Rollercoaster_Ride.py
@@ -17,14 +17,27 @@ if height > 120:
     print("You can ride the rollercoaster!")
 
     age = int(input("What is your age?\n"))
+    pic_consent = input("Do you want the pictures too?\n")
 
     if age < 12:
-        print("You have to pay $5 for the ride")
+
+        if pic_consent == "Yes" :
+            print("You have to pay $8 for the ride")
+        else:
+            print("You have to pay $5 for the ride.")
 
     elif age > 18:
-        print("You have to pay $12 for the ride.")
+
+        if pic_consent == "Yes" :
+            print("You have to pay $15 for the ride")
+        else:
+            print("You have to pay $12 for the ride.")
 
     else:
-        print("You have to pay $7 for the ride.")
+        if pic_consent == "Yes" :
+            print("You have to pay $10 for the ride")
+        else:
+            print("You have to pay $7 for the ride.")
+
 else:
     print("You can't ride the rollercoaster!")
