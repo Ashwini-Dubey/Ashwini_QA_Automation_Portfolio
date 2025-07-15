9ee02d1251752d8ab76dbdcd4f253c07383078ab
Ashwini Dubey <ak213414@gmail.com>
2025-07-16 00:25:43 +0530

    LearnCode_CoinFlipGenerator

--- 01_Python_Basics/L01_RandomNumberGenerator.py
+++ 01_Python_Basics/L01_RandomNumberGenerator.py
@@ -1,3 +1,17 @@
+"""
+This script demonstrates the use of the 'random' module in Python to generate different types of random numbers.
+
+Functions Used:
+- random.randint(a, b): Returns a random integer N such that a <= N <= b.
+- random.random(): Returns a random float number between 0.0 and 1.0.
+- random.uniform(a, b): Returns a random float N such that a <= N <= b.
+
+Output:
+- A random integer between 1 and 100.
+- A random float between 0.0 and 1.0.
+- A random float between 1 and 10.
+"""
+
 import random
 
 random_integer = random.randint(1,100)
@@ -5,3 +19,17 @@ print(random_integer)
 
 random_float_0_to_1 = random.random()
 print(random_float_0_to_1)
+
+random_uniform = random.uniform(1,10)
+print(random_uniform)
+
+# Coin Flip Simulator
+#
+# This script uses Python's random module to simulate a coin toss.
+# It randomly prints either "Heads" or "Tails" each time it is run.
+
+coin_toss = random.randint(0,100)
+if coin_toss == 0:
+    print("Heads")
+else:
+    print("Tails")
