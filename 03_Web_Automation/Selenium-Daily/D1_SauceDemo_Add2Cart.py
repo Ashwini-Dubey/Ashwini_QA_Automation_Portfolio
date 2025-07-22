bafe6364f436bcf3c18c0fd375da45c582d72649
Ashwini Dubey <ak213414@gmail.com>
2025-07-22 23:28:31 +0530

    AutomationChallenge_Day1

--- 03_Web_Automation/Selenium-Daily/D1_SauceDemo_Add2Cart.py
+++ 03_Web_Automation/Selenium-Daily/D1_SauceDemo_Add2Cart.py
@@ -32,48 +32,67 @@ from selenium.webdriver.common.by import By
 
 Base_URL = "https://www.saucedemo.com/"
 
-#Step1: Launches the SauceDemo login page
+
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.implicitly_wait(30)
-driver.get(Base_URL)
-current_url = driver.current_url
-assert current_url == Base_URL, "Website Launch failed!"
-print("Website Launched Successfully!")
+
+#Step1: Launches the SauceDemo login page
+def Session_Init():
+    driver.get(Base_URL)
+    current_url = driver.current_url
+    assert current_url == Base_URL, "Website Launch failed!"
+    print("Website Launched Successfully!")
+    return driver
 
 #Step2: Logs in using standard user credentials
-driver.find_element(By.ID,"user-name").send_keys("standard_user")
-driver.find_element(By.ID,"password").send_keys("secret_sauce")
-driver.find_element(By.ID,"login-button").click()
-assert "inventory.html" in driver.current_url, "Login failed!"
-print("Login successful!")
+def User_Login():
+    driver.find_element(By.ID,"user-name").send_keys("standard_user")
+    driver.find_element(By.ID,"password").send_keys("secret_sauce")
+    driver.find_element(By.ID,"login-button").click()
+    assert "inventory.html" in driver.current_url, "Login failed!"
+    print("Login successful!")
 
 #Step3: Adds the first two products from the inventory to the cart
-driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
-driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
-assert "Remove" in driver.find_element(By.ID,"remove-sauce-labs-backpack").text, "Backpack not added to the cart!"
-print("Backpack added to cart!")
-assert "Remove" in driver.find_element(By.ID,"remove-sauce-labs-bike-light").text, "Bike light not added to the cart!"
-print("Bike light added to cart!")
-assert "2" in driver.find_element(By.CSS_SELECTOR,".shopping_cart_badge").text,"Products not added to the cart."
-print("Products are added to cart!")
+def Add_Product_To_Cart():
+    driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(By.ID,"add-to-cart-sauce-labs-bike-light").click()
+    assert "Remove" in driver.find_element(By.ID,"remove-sauce-labs-backpack").text, "Backpack not added to the cart!"
+    print("Backpack added to cart!")
+    assert "Remove" in driver.find_element(By.ID,"remove-sauce-labs-bike-light").text, "Bike light not added to the cart!"
+    print("Bike light added to cart!")
+    assert "2" in driver.find_element(By.CSS_SELECTOR,".shopping_cart_badge").text,"Products not added to the cart."
+    print("Products are added to cart!")
 
 '''
 Step4: Navigates to the cart and verifies:
    - Both selected products are listed
    - The cart icon displays the correct item count
 '''
-driver.find_element(By.ID,"shopping_cart_container").click()
-assert "cart.html" in driver.current_url, "Navigation to cart failed!"
-print("Navigation to card successful!")
-assert "2" in driver.find_element(By.CSS_SELECTOR,".shopping_cart_badge").text, "Cart is having incorrect product count!"
-print("Cart contains correct count of products.")
-assert "Backpack" and "Bike Light" in driver.find_element(By.ID,"cart_contents_container").text,"Incorrect Products are in cart."
-print("Correct products in the cart.")
+def Cart_Validation():
+    driver.find_element(By.ID,"shopping_cart_container").click()
+    assert "cart.html" in driver.current_url, "Navigation to cart failed!"
+    print("Navigation to card successful!")
+    assert "2" in driver.find_element(By.CSS_SELECTOR,".shopping_cart_badge").text, "Cart is having incorrect product count!"
+    print("Cart contains correct count of products.")
+    cart_text = driver.find_element(By.ID, "cart_contents_container").text
+    assert "Backpack" in cart_text and "Bike Light" in cart_text,"Incorrect Products are in cart."
+    print("Correct products in the cart.")
 
 #Step5: Takes a screenshot of the cart page.
-timestamp = datetime.now()
-driver.save_screenshot(f"Cart_{timestamp}")
+def Cart_Screenshot():
+    timestamp = datetime.now()
+    driver.save_screenshot(f"Cart_{timestamp}.png")
 
 #Step6: Closes the browser session
-driver.close()
+def Session_Close():
+    driver.close()
+
+if __name__ == '__main__':
+    Session_Init()
+    User_Login()
+    Add_Product_To_Cart()
+    Cart_Validation()
+    Cart_Screenshot()
+    Session_Close()
